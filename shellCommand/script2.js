ff14580daae56fd7c410a2cf31c51050660e939a
grosfaignan <grosfaignan@gmail.com>
2020-01-12 19:42:29 +0100

    first commit for the netshell php code source

---
+++ shellCommand/script2.js
@@ -0,0 +1,22 @@
+
+// Get the input field
+var input = document.getElementById("cmd");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("ok").click();
+  }
+});
+
+function getSrc()
+{
+    document.getElementById('shellFrame').src ="shell.php?cmd=" + document.getElementById('cmd').value + "&regex="+ document.getElementById('regex').value;;
+    
+}
+
+
---
+++ shellCommand/shell.php
@@ -0,0 +1,43 @@
+<?php
+function display($data)
+{
+	echo '<!doctype html><html lang="fr"><head><meta charset="utf-8"><title>Titre de la page</title><link rel="stylesheet" href="stdout.css"></head><body>';
+	echo '<div id="c1"><pre>';
+	for ($i = 0; $i < count($data); $i++) {
+		echo ($i . '<br/>');
+	}
+	echo '</pre></div><div id="c2"><pre>';
+
+	for ($i = 0; $i < count($data); $i++) {
+		echo ($data[$i] . '<br/>');
+	}
+
+	echo '</pre></div></body></html>';
+
+}
+function parseStdout($data)
+{
+
+	$temp   = htmlspecialchars($data);
+	$split1 = preg_split("/(\s+\n*)/U", $temp); //"/\s{2,}|\n/","/(\s\s+)|(\n)/U" , une version pas trop mal "/(0\s+\n*)/U" avec 0=null, \s+ pour les espace et \n pour les retour chariot
+	$split2 = preg_split("/\n/", $temp);
+	return $split2;
+
+}
+
+function shellCommand($stringCommand)
+{
+
+	$directReturn = shell_exec(escapeshellcmd($stringCommand));
+	$cleanReturn  = mb_convert_encoding($directReturn, 'UTF-8', 'CP-850');
+	return $cleanReturn;
+}
+//$cmd    = shellCommand('help'); // for debug
+
+$input  = $_GET;
+var_dump($input);
+$cmd    = shellcommand($input['cmd']);
+$parsed = parseStdout($cmd);
+display($parsed);
+
+?>
